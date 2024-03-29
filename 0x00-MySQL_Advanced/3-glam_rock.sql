a798f9aa546b9a1c6e312bada10fa6c6f8665bd8
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2024-03-29 10:50:23 +0000

    3. Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- List all bands with Glam rock as their main style, ranked by their longevity
--- Column names are `band_name` and `lifespan` (in years until 2022)
+-- Column names are `band_name` and `lifespan`
 -- Attributes `formed` and `split` are used for computing the lifespan
 SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
 FROM metal_bands
